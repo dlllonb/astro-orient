57ea36c5af5a915bc745d6c4c0ba6f78a2ccaa96
keyagarg <gpp20143@gmail.com>
2026-06-02 13:18:03 -0400

    Testing with interpreting GPS data

--- GPS_IMU_Camera_Programs/GPS/GPS_test_program.py
+++ GPS_IMU_Camera_Programs/GPS/GPS_test_program.py
@@ -1,8 +1,35 @@
 import serial
+import pynmea2
+# ser = serial.Serial("COM4", baudrate=9600, timeout=3000)
+#
+# datalist = []
+# for i in range(30):
+#     data = ser.readline()
+#     if len(data) > 0:
+#         datalist.append(data.decode("utf-8"))
+# ser.close()
+#
+# logFile = open("GPS_test.txt","w")
+# for data in datalist:
+#     # Convert tuple to string, strips parenthesis, and adds new line before writing to file.
+#     logFile.write(str(data).strip('\n'))
+#
+# logFile.close()
 
-ser = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=3000)
-while True:
-    data = ser.readline()
-    if len(data) > 0:
-        print(data.decode('utf-8'))
-ser.close()
+file = open('GPS_test.txt', encoding='utf-8')
+
+for line in file.readlines():
+    try:
+        msg = pynmea2.parse(line)
+        #print(type(msg))
+        if isinstance(msg, pynmea2.types.talker.GGA):
+            print(msg.timestamp)
+            print(msg.lat, msg.lat_dir)
+            print(msg.lon, msg.lon_dir)
+            print()
+
+        #print(repr(msg))
+
+    except pynmea2.ParseError as e:
+        print('Parse error: {}'.format(e))
+        continue
